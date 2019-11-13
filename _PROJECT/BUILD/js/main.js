890b8911bf3839c0e93e3229cc135f30eaf7e1dc
Sharamoff <sharamoff@gmail.com>
2019-11-13 15:26:15 +0300

    index.html v.1

--- _PROJECT/BUILD/js/main.js
+++ _PROJECT/BUILD/js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
 $('#id_idx_fullpage').fullpage({
 	scrollBar: true, // нужно для wow
-	responsiveWidth: 815,
+	responsiveWidth: 1025,
 	menu: '#id_idx_nav',
 	anchors: ['id_collage', 'id_agency', 'id_services', 'id_solutions', 'id_projects', 'id_clients', 'id_contacts'],
 	navigation: true,
@@ -34,11 +34,11 @@ $('.block-project img').click( function() {
 	$('.project__note').fadeOut();
 	var note = $(this).attr('rel');
 	$('#id_' + note).fadeIn();
-})
+});
 
 $('.project__note').click( function() {
 	$(this).fadeOut();
-})
+});
 var wow = new WOW(
 	{
 		boxClass:     'wow',
--- _PROJECT/SRC/js/base/function.js
+++ _PROJECT/SRC/js/base/function.js
@@ -3,7 +3,7 @@
 
 $('#id_idx_fullpage').fullpage({
 	scrollBar: true, // нужно для wow
-	responsiveWidth: 815,
+	responsiveWidth: 1025,
 	menu: '#id_idx_nav',
 	anchors: ['id_collage', 'id_agency', 'id_services', 'id_solutions', 'id_projects', 'id_clients', 'id_contacts'],
 	navigation: true,
@@ -31,8 +31,11 @@ $('.block-project img').click( function() {
 	$('.project__note').fadeOut();
 	var note = $(this).attr('rel');
 	$('#id_' + note).fadeIn();
-})
+});
 
 $('.project__note').click( function() {
 	$(this).fadeOut();
-})
+});
+
+
+
